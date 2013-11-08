234e4118e0341d8174076fa6b6e66b0243801704
Dan Nuffer <danielnuffer@gmail.com>
2013-11-08 10:58:23 -0700

    Add pull_me.py

---
+++ pull_me.py
@@ -0,0 +1,15 @@
+from random import randint
+from time import sleep
+from os import system
+from easygui import msgbox
+
+while True:
+  time.sleep(randint(30, 600))
+  #for _ in range(5):
+  #  print "\a"
+  #  time.sleep(.2)
+  system("aplay /usr/lib/libreoffice/share/gallery/sounds/kongas.wav")
+  msgbox("Hi Dan", "Time is up")
+  
+
+
