7a9195b189716e4e6bacfc2061fac51a5f587756
Dan Nuffer <danielnuffer@gmail.com>
2013-11-14 04:36:28 -0700

    Remove commented code.

--- pull_me.py
+++ pull_me.py
@@ -5,12 +5,7 @@ from os import system
 from easygui import msgbox
 
 while True:
-  sleep(randint(30, 600))
-  #for _ in range(5):
-  #  print "\a"
-  #  time.sleep(.2)
+  delay = randint(30, 1100)
+  sleep(delay)
   system("aplay /usr/lib/libreoffice/share/gallery/sounds/kongas.wav")
   msgbox("Hi Dan", "Time is up")
-  
-
-
