4eecac0764e8abfc33c9e77b8eb6b700b536f1a0
Dan Nuffer <danielnuffer@gmail.com>
2014-09-04 07:33:15 -0600

    Use Glass.wav if it exists.

--- pull_me.py
+++ pull_me.py
@@ -2,10 +2,14 @@
 from random import randint
 from time import sleep
 from os import system
+import os.path
 from easygui import msgbox
 
 while True:
   delay = randint(60, 2000)
   sleep(delay)
-  system("aplay /usr/lib/libreoffice/share/gallery/sounds/kongas.wav")
+  if os.path.isfile("/usr/share/sounds/GNUstep/Glass.wav"):
+    system("aplay /usr/share/sounds/GNUstep/Glass.wav")
+  else:
+    system("aplay /usr/lib/libreoffice/share/gallery/sounds/kongas.wav")
   msgbox("Hi Dan", "Time is up")
